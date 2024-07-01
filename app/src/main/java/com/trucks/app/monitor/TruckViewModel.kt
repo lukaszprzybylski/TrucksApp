d0429135bfbc1c8dc63d68c9028f6499f2fae526
lukaszprzybylski <przybylski.lukasz@o2.pl>
2024-07-01 12:22:53 +0200

    Refactor

--- app/src/main/java/com/trucks/app/monitor/TruckViewModel.kt
+++ app/src/main/java/com/trucks/app/monitor/TruckViewModel.kt
@@ -25,8 +25,7 @@ class TruckViewModel @Inject constructor(
     private val _hideNotification = MutableStateFlow(false)
     val hideNotification: StateFlow<Boolean> get() = _hideNotification.asStateFlow()
 
-    private var truckResponse: Result<TruckResponse>? = null
-    var inRadiusCount: Int = 0
+     var inRadiusCount: Int = 0
     private var periodicJob: Job? = null
 
     init {
@@ -35,7 +34,7 @@ class TruckViewModel @Inject constructor(
 
     fun setAppInBackground(isInBackground: Boolean) {
         if (isInBackground) {
-            inRadiusCount = getTruckByDistance(truckResponse).count { it.inRange }
+            inRadiusCount = _vehicles.value?.count { it.inRange } ?: 0
             viewModelScope.launch {
                 delay(DELAY_TIME_BACKGROUND)
                 periodicJob?.cancel()
@@ -50,8 +49,7 @@ class TruckViewModel @Inject constructor(
     private fun fetchTrucksData() {
         viewModelScope.launch {
             try {
-                truckResponse = getAvailableTrucksUseCase()
-                val vehicleList = getTruckByDistance(truckResponse)
+                val vehicleList = getAvailableTrucksUseCase()
                 _vehicles.value = vehicleList
             } catch (e: Exception) {
                 // Handle the error appropriately
@@ -69,31 +67,13 @@ class TruckViewModel @Inject constructor(
         }
     }
 
-    private fun getTruckByDistance(result: Result<TruckResponse>?): List<TruckItem> {
-        val itemList = mutableListOf<TruckItem>()
-        result?.data?.forEach {
-            val vehicleLocation = Location("").apply {
-                latitude = it.location.component2()
-                longitude = it.location.component1()
-            }
-            val isInRadius = TARGET_LOCATION.distanceTo(vehicleLocation) <= DISTANCE
-            itemList.add(TruckItem(it.vehicleId, isInRadius))
-        }
-        return itemList
-    }
-
     override fun onCleared() {
         super.onCleared()
         periodicJob?.cancel()
     }
 
     companion object {
-        const val DISTANCE = 1000 // 1km
         const val DELAY_TIME = 10000L // Refresh every 10 seconds
         const val DELAY_TIME_BACKGROUND = 60000L // 60 seconds
-        val TARGET_LOCATION = Location("").apply {
-            latitude = 46.5223916
-            longitude = 6.6314437
-        }
     }
 }
